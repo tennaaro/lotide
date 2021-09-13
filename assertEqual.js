55e99e2266fe19c1e1201631e8788b47705a06e9
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-13 14:34:40 -0400

    assertEquals

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀😀Assertion Passed: " + actual + " === " + expected);
+  }
+  else {
+    console.log("😢😢😢Assertion Failed: " + actual + " !== " + expected);
+  }
+  //console.assert(actual === expected, "😢😢😢" + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
