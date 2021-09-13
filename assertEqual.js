66c963cabbc7ecdddc9d4e17d91330688f0262f2
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-13 14:48:18 -0400

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😀😀😀Assertion Passed: " + actual + " === " + expected);
+    console.log(`😀😀😀Assertion Passed: ${actual} === + ${expected}`);
   }
   else {
-    console.log("😢😢😢Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
   }
   //console.assert(actual === expected, "😢😢😢" + actual + " !== " + expected);
 };
