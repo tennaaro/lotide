1a5dd4e56a3412b62fbcc39d859278441eaf9d9a
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-15 14:05:15 -0400

    changed test cases

--- countOnly.js
+++ countOnly.js
@@ -41,13 +41,14 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
-
   return results;
 }
 
@@ -68,4 +69,4 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], 1);
+assertEqual(result1["Agouhanna"], undefined);
