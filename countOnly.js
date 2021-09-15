552dc473e4f973d0d96d74c60defe4f18d1903dc
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-15 14:00:46 -0400

    adding countOnly

---
+++ countOnly.js
@@ -0,0 +1,71 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  }
+  else {
+    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+  //console.assert(actual === expected, "😢😢😢" + actual + " !== " + expected);
+};
+
+const eqArrays = function(arr1, arr2) {
+  //case where array lengths don't match
+  if (arr1.length !== arr2.length) {
+    return "Input two arrays of same length."
+  }
+  let rval = true
+  //loop through each respective value of arrays
+  for (let i = 0; i < arr1.length; i++) {
+    //compare using assert equal
+    if (!assertEqual(arr1[i], arr2[i])) {
+      rval = false;
+    }
+  }
+  return rval;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if(eqArrays(array1, array2)) {
+    console.log("✅✅✅ Arrays are equal.");
+  }
+  else {
+    console.log("❌❌❌ Arrays not equal.");
+  }
+}
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], 1);
