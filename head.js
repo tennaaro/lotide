053844c91ed58ded0f6b3fbf9332aae0706deb0d
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-20 18:23:25 -0400

    refractoring head

--- head.js
+++ head.js
@@ -1,10 +1,9 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = require('./assertEqual');
+//const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
