02585aab38deb030ada4f0b023f826a1194436d1
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-20 17:50:53 -0400

    regractoring assertArrayEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,7 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  }
-  else {
-    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-  //console.assert(actual === expected, "😢😢😢" + actual + " !== " + expected);
-};
-
-const eqArrays = function(arr1, arr2) {
-  //case where array lengths don't match
-  if (arr1.length !== arr2.length) {
-    return "Input two arrays of same length."
-  }
-  let rval = true
-  //loop through each respective value of arrays
-  for (let i = 0; i < arr1.length; i++) {
-    //compare using assert equal
-    if (!assertEqual(arr1[i], arr2[i])) {
-      rval = false;
-    }
-  }
-  return rval;
-}
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const assertArraysEqual = function(array1, array2) {
   if(eqArrays(array1, array2)) {
@@ -37,9 +12,11 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
+
 //console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 //console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 //console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
 
-//console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
-//console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
+module.exports = assertArraysEqual;
