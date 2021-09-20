474ca544494a62774a49a38d7ec8c16e9a80e3d7
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-20 17:33:15 -0400

    refractor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,5 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  }
-  else {
-    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-  //console.assert(actual === expected, "😢😢😢" + actual + " !== " + expected);
-};
 
 const eqArrays = function(arr1, arr2) {
   //case where array lengths don't match
@@ -21,12 +10,14 @@ const eqArrays = function(arr1, arr2) {
   //loop through each respective value of arrays
   for (let i = 0; i < arr1.length; i++) {
     //compare using assert equal
-    if (!assertEqual(arr1[i], arr2[i])) {
+    if (!(arr1[i] === arr2[i])) {
       rval = false;
     }
   }
   return rval;
 }
+
+module.exports = eqArrays;
 
 //console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 //console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
